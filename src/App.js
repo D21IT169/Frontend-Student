0c6a20de447b46bebbc0c2a106e492074143d000
D21IT169 <89838262+D21IT169@users.noreply.github.com>
2022-09-20 12:35:26 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyAlert from "./components/MyAlert";
 import { useState } from "react";
 
-function App() {
-  const [alert, setAlert] = useState(null);
 
-  let showAlert = (type, message) => {
-    setAlert({
-      type: type,
-      message: message,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  };
 
   return (
     <div className="App">
